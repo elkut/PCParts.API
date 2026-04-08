c665331f81393eff404754e97f8610c7612406a2
Alkut Askar <elkut.esker@gmail.com>
2026-04-08 08:25:00 -0400

    Add project files.

---
+++ Model/PartsSummaryDto.cs
@@ -0,0 +1,14 @@
+﻿namespace PCParts.API.Model
+{
+    public class PartsSummaryDto
+    {
+        public int Id { get; set; }
+        public string? name { get; set; }
+        public string? brand { get; set; }
+        public string? category { get; set; }
+        public double price { get; set; }
+        public long thumbnailUrl { get; set; }
+        public bool inStock { get; set; }
+
+    }
+}
