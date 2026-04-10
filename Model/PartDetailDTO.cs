d661ca4af7d85bcc0664e5ee5c156baf6bddb9d6
Alkut Askar <elkut.esker@gmail.com>
2026-04-10 08:30:25 -0400

    Added more properties to details DTOs

---
+++ Model/PartDetailDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace PCParts.API.Model
+{
+    public class PartDetailDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double? Price { get; set; }
+        public double? LowestPriceInLast30Days { get; set; }
+        public string? Description { get; set; }
+        public string[]? ImageUrls { get; set; }
+
+    }
+}
--- Model/PartsSummaryDto.cs
+++ Model/PartsSummaryDto.cs
@@ -3,12 +3,14 @@
     public class PartsSummaryDto
     {
         public int Id { get; set; }
-        public string? name { get; set; }
-        public string? brand { get; set; }
-        public string? category { get; set; }
-        public double price { get; set; }
-        public long thumbnailUrl { get; set; }
-        public bool inStock { get; set; }
+        public string? Name { get; set; }
+        public string? Brand { get; set; }
+        public string? Category { get; set; }
+        public double Price { get; set; }
+        public string? ThumbnailUrl { get; set; }
+        public bool InStock { get; set; }
 
+
+        public ICollection<PartDetailDTO> PartDetails { get; set; } = new List<PartDetailDTO>();
     }
 }
