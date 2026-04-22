c51cce33abd5790757b653ce8fdf9b8e846bd2a1
Alkut Askar <elkut.esker@gmail.com>
2026-04-21 21:54:19 -0400

    completed PCPartsController

--- Controllers/PCPartsController.cs
+++ Controllers/PCPartsController.cs
@@ -7,12 +7,35 @@ namespace PCParts.API.Controllers
     [ApiController]
     public class PCPartsController : ControllerBase
     {
+
+        // Returns single object
+        //[HttpGet]
+        //public ActionResult<PartsSummaryDto> GetPCParts()
+        //{
+        //    var pcparts = PCPartsDataStore.Parts.PCParts;
+
+        //    return Ok(pcparts);
+        //}
+
+        // Tells consumers (Swagger, clients, other devs) to expect an array, not a single object
         [HttpGet]
         public ActionResult<IEnumerable<PartsSummaryDto>> GetPCParts()
         {
             var pcparts = PCPartsDataStore.Parts.PCParts;
 
             return Ok(pcparts);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<PartsSummaryDto> GetPCPart(int id)
+        {
+            var pcpart = PCPartsDataStore.Parts.PCParts.FirstOrDefault(part => part.Id == id);
+
+            if (pcpart == null)
+            {
+                return NotFound("Parts Not Found");
+            }
+            return Ok(pcpart);
         }
 
     }
---
+++ Controllers/PartsDetailsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PCParts.API.Models;
+
+namespace PCParts.API.Controllers
+{
+    [Route("api/pcparts/{Id}/pcpartdetails")]
+    [ApiController]
+    public class PartsDetailsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PartDetailDTO>> GetPartDetail(int partId)
+        {
+            var details = PCPartsDataStore.Parts.PCParts.FirstOrDefault(d => d.Id == partId);
+
+            if (details == null)
+            {
+                return NotFound();
+            }
+            return Ok(details.PartDetails);
+        }
+    }
+}
