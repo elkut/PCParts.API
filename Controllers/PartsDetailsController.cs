c029d6383b7e8d94cec267547cb10a003e3c2d92
Alkut Askar <elkut.esker@gmail.com>
2026-04-22 22:09:59 -0400

    updated parts details controller

--- Controllers/PartsDetailsController.cs
+++ Controllers/PartsDetailsController.cs
@@ -8,7 +8,7 @@ namespace PCParts.API.Controllers
     public class PartsDetailsController : ControllerBase
     {
         [HttpGet]
-        public ActionResult<IEnumerable<PartDetailDTO>> GetPartDetail(int partId)
+        public ActionResult<IEnumerable<PartDetailDTO>> GetPartsDetails(int partId)
         {
             var details = PCPartsDataStore.Parts.PCParts.FirstOrDefault(d => d.Id == partId);
 
@@ -17,6 +17,26 @@ namespace PCParts.API.Controllers
                 return NotFound();
             }
             return Ok(details.PartDetails);
+        }
+
+        [HttpGet("{partdetailId}")]
+        public ActionResult<PartDetailDTO> GetPartDetails(int pcPartId, int partdetailId)
+        {
+            var pcpart = PCPartsDataStore.Parts.PCParts.FirstOrDefault((part) => part.Id == pcPartId);
+
+            if (pcpart == null)
+            {
+                return NotFound("Parts Not Found");
+            }
+
+            var pcPartDetail = pcpart.PartDetails.FirstOrDefault(p => p.Id == partdetailId);
+
+            if (pcPartDetail == null)
+            {
+                return NotFound("Parts Not Found");
+            }
+
+            return Ok(pcPartDetail);
         }
     }
 }
